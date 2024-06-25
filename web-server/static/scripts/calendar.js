1fbcfb8ff8e0337b6963742122d478dfb6440bcb
nikhegg <77997488+nikhegg@users.noreply.github.com>
2024-06-25 11:09:28 +0200

    Add refresh button anims

--- web-server/static/scripts/calendar.js
+++ web-server/static/scripts/calendar.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", event => {
+    const refreshBtn = document.getElementById("refresh")
+    let refreshClicked = false
+    refreshBtn.addEventListener("click", () => {
+        if(refreshClicked) return
+        refreshBtn.style.animation = "refresh-rotate 1s infinite"
+        refreshClicked = true
+        // TODO Do some sync stuff
+        
+        // Temporary anim solution
+        setTimeout(() => {
+            refreshBtn.style.animation = "none"
+            refreshClicked = false
+        }, 2000)
+    })
+
     console.log("Calendar script loaded")
 })
