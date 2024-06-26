7a0effd481636bd469f645d17e01d66555de0000
FrixhELL <sarapovrodion@gmail.com>
2024-06-26 17:46:06 +0200

    Add tgbot buttons

--- bots-server/notif_tgbot.py
+++ bots-server/notif_tgbot.py
@@ -1,13 +1,14 @@
 import telebot
 import webbrowser
+from telebot import types
 
 with open('credentials.TXT', 'r') as file:
     bot_token = file.read()
 bot = telebot.TeleBot(bot_token)
-
-# @bot.message_handler(commands=['website', 'site'])
-# def site(message):
-#     webbrowser.open('url')
+commands = bot.get_my_commands()
+@bot.message_handler(commands=['website', 'site'])
+def site(message):
+    webbrowser.open('url')
 @bot.message_handler(commands=['start'])
 def start(message):
 
@@ -19,10 +20,31 @@ def start(message):
     else:
         user_name = f'{first_name} {last_name}'
 
+    markup = types.ReplyKeyboardMarkup()
+    sun_btn = types.KeyboardButton('Подписаться')
+    unsub_btn = types.KeyboardButton('Отписаться')
+    markup.row(sun_btn, unsub_btn)
+    help_btn = types.KeyboardButton('Список команд')
+    markup.row(help_btn)
+    bot.register_next_step_handler(message, on_click)
+
     bot.send_message(message.chat.id, f'Привет, {user_name}!\n'
                                       'Для подписки на рассылку воспользуйся командой /sub\n'
                                       'Остальные команды доступны в /help',
-                     parse_mode='Markdown')
+                     parse_mode='Markdown', reply_markup=markup)
+
+def on_click(message):
+    if message.text == 'Подписаться':
+        bot.send_message(message.chat.id, 'Вы успешно подписались на рассылку!')
+        bot.register_next_step_handler(message, on_click)
+    elif message.text == 'Отписаться':
+        bot.send_message(message.chat.id, 'Вы успешно отписались от рассылки!')
+        bot.register_next_step_handler(message, on_click)
+    elif message.text == 'Список команд':
+        bot.send_message(message.chat.id, 'Список команд:\n')
+        for command in commands:
+            bot.send_message(message.chat.id, f"/{command.command} - {command.description}\n")
+        bot.register_next_step_handler(message, on_click)
 
 
 # @bot.message_handler(commands=['stop'])
