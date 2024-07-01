13b4b7a951fb578f2379d7df98c66ccc65185869
Timoshchenko Nikolay <silent_master@mail.ru>
2024-07-01 19:17:32 +0200

    add socket-server on server.py

--- web-server/src/database.py
+++ web-server/src/database.py
@@ -1,41 +1,14 @@
 import sqlite3
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 class Database:
-    def __init__(self):
-        db_path = os.getenv('DATABASE_PATH')
-        if not db_path:
-            raise ValueError("DATABASE_PATH not set in environment variables")
+    def __init__(self, db_path):
         self.conn = sqlite3.connect(db_path)
         self.cursor = self.conn.cursor()
 
+    def query(self, sql):
+        self.cursor.execute(sql)
+        return self.cursor.fetchall()
+
     def close(self):
         self.conn.close()
-
-    # Метод для добавления события
-    def add_event(self, title, description, category, event_time):
-        self.cursor.execute('''
-        INSERT INTO events (title, description, category, event_time)
-        VALUES (?, ?, ?, ?)
-        ''', (title, description, category, event_time))
-        self.conn.commit()
-
-    # Метод для добавления подписчика в телеге
-    def add_telegram_subscriber(self, telegram_id, category):
-        self.cursor.execute('''
-        INSERT INTO telegram_subscribers (id, category)
-        VALUES (?, ?)
-        ''', (telegram_id, category))
-        self.conn.commit()
-
-    # Метод для добавления email-подписчика
-    def add_email_subscriber(self, email, category):
-        self.cursor.execute('''
-        INSERT INTO email_subscribers (email, category)
-        VALUES (?, ?)
-        ''', (email, category))
-        self.conn.commit()
--- web-server/src/server.py
+++ web-server/src/server.py
@@ -1,11 +1,50 @@
+import os
+import socket
 import socketserver
+import threading
 from request_handler import RequestHandler
 from database import Database
+from dotenv import load_dotenv
 
-PORT = 8000
-Handler = RequestHandler
-db = Database()
+HOST, PORT = "185.16.137.205", 8000
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+load_dotenv()
+db_path = os.getenv('DATABASE_PATH')
+if not db_path:
+    raise ValueError("DATABASE_PATH not set in environment variables")
+
+db = Database(db_path)
+handler = RequestHandler
+handler.connect_database(db)
+
+
+def start_http_server():
+    with socketserver.TCPServer(("", PORT), handler) as httpd:
+        print(f"\nServing at port {PORT}")
+        httpd.serve_forever()
+
+
+def start_socket_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(("", PORT + 1))
+        s.listen()
+        print(f"\nSocket server listening on port {PORT + 1}")
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+
+
+if __name__ == "__main__":
+    http_server_thread = threading.Thread(target=start_http_server)
+    socket_server_thread = threading.Thread(target=start_socket_server)
+
+    http_server_thread.start()
+    socket_server_thread.start()
+
+    http_server_thread.join()
+    socket_server_thread.join()
