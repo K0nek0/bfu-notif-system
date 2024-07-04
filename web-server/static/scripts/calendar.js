0803676a36aa480b890d21eaae060e57c03c86f4
nikhegg <77997488+nikhegg@users.noreply.github.com>
2024-07-04 14:33:26 +0200

    Change category names
    Now they are similar to those that server sends

--- web-server/static/scripts/calendar.js
+++ web-server/static/scripts/calendar.js
@@ -91,19 +91,10 @@ document.addEventListener("DOMContentLoaded", async event => {
         eventsCarousel.scrollLeft += cardWidth + gap
     })
 
-    const categories = {
-        "important": {
-            name: "Важное",
-            color: "#F07427"
-        },
-        "event": {
-            name: "Мероприятие",
-            color: "#36D593"
-        },
-        "study": {
-            name: "Обучение",
-            color: "#627CFF"
-        }
+    const categoriesColors = {
+        "Важное": "#F07427",
+        "Мероприятие": "#36D593",
+        "Обучение": "#627CFF"
     }
 
     function createEventCard(title, text, timestamp, category) {
@@ -120,8 +111,9 @@ document.addEventListener("DOMContentLoaded", async event => {
         eventDate.className = "event-date"
 
         const colorLine = document.createElement("div")
-        let color = categories[category].color
-        if(!categories[category] || !categories[category].color) color = "#00AAF9" // Default Event Card Color
+        let color
+        if(!categoriesColors[category]) color = "#00AAF9" // Default Event Card Color
+        else color = categoriesColors[category]
         colorLine.style.background = color
         colorLine.className = "color-line"
         eventDate.append(colorLine)
@@ -147,7 +139,7 @@ document.addEventListener("DOMContentLoaded", async event => {
         const eventCategory = document.createElement("div")
         eventCategory.className = "event-category"
         let displayCategory = ""
-        if(categories[category] && categories[category].name) displayCategory = categories[category].name
+        displayCategory = category
         eventCategory.textContent = displayCategory
         eventDate.append(eventCategory)
 
